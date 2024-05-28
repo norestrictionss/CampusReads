5122501579afcebe4aeb2ea2a27ec1f66a691f65
iremkiranmezar <102966343+Krnmzr@users.noreply.github.com>
2024-05-28 07:36:54 +0300

    form design update

--- frontend/src/pages/EditProfile.js
+++ frontend/src/pages/EditProfile.js
@@ -1,66 +1,111 @@
 import React, { useState } from "react";
 import "../Profile.css"; // Import your CSS file for styling
+import { db, auth } from "../config/firebase"; // Import Firebase configuration
+import { doc, updateDoc } from "firebase/firestore";
+import { updateProfile, updateEmail, updatePassword } from "firebase/auth";
 
 export default function EditProfile() {
+    const [password, setPassword] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [gender, setGender] = useState("");
+    const [email, setEmail] = useState("");
+    const [department, setDepartment] = useState("");
 
-// State variables for registration fields
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
-const [phoneNumber, setPhoneNumber] = useState("");
-const [gender, setGender] = useState("");
-const [email, setEmail] = useState("");
-const [department, setDepartment] = useState("");
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-// Function to handle form submission
-const handleSubmit = (event) => {
-  event.preventDefault();
-  // Here you can implement your registration logic using the form fields
-  // For example, you can send an API request to register the user
-  console.log("Username:", username);
-  console.log("Password:", password);
-  console.log("Phone Number:", phoneNumber);
-  console.log("Gender:", gender);
-  console.log("Email:", email);
-  console.log("Department:", department);
-  // Reset the form fields after submission if needed
-  setUsername("");
-  setPassword("");
-  setPhoneNumber("");
-  setGender("");
-  setEmail("");
-  setDepartment("");
-};
+        // Reset form fields
+        setPassword("");
+        setPhoneNumber("");
+        setGender("");
+        setEmail("");
+        setDepartment("");
+    };
 
     return (
-
         <div className="addForm-container rounded mt-5 mb-5">
             <div className="row">
                 <div className="col-md-10 mx-auto">
                     <div className="p-3 py-5">
                         <div className="d-flex justify-content-between align-items-center mb-3">
-                            <h4 className="text-right" > <strong>Edit Your Profile</strong></h4>
+                            <h4 className="text-right"><strong>Edit Your Profile</strong></h4>
                         </div>
                         <form className="row mt-3" onSubmit={handleSubmit}>
                             <div className="mb-3">
-                                <label htmlFor="formFile" className="form-label" >Profile Image</label>
+                                <label htmlFor="formFile" className="form-label">Profile Image</label>
                                 <input className="form-control" name="imageUrl" type="file" id="formFile" />
                             </div>
-                            <div className="col-md-12"><label htmlFor="username" className="labels">Username</label><input name="username" type="text" className="form-control" placeholder="" value="" /></div>
-                            <div className="col-md-12"><label htmlFor="password" className="labels">Password</label><input name="userPassword" type="password" className="form-control" placeholder="" value="" /></div>
-                            <div className="col-md-12"><label className="labels">Phone Number</label><input name="userPhoneNumber" type="phone" className="form-control" placeholder="" value="" /></div>
-                            <div className="col-md-12"><label className="labels">Gender</label><select>
-                                <option value="">Select Gender</option>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                                <option value="other">Other</option>
-                            </select></div>
-                            <div className="col-md-12"><label className="labels">Email</label><input name="userEmail" type="email" className="form-control" placeholder="" value="" /></div>
-                            <div className="col-md-12"><label className="labels">Department</label><input name="userDepartment" type="text" className="form-control" placeholder="" value="" /></div>
+                            
+                            <div className="col-md-12">
+                                <label htmlFor="password" className="labels">Password</label>
+                                <input
+                                    name="userPassword"
+                                    type="password"
+                                    id="password"
+                                    className="form-control"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+                            <div className="col-md-12">
+                                <label className="labels">Phone Number</label>
+                                <input
+                                    name="userPhoneNumber"
+                                    type="phone"
+                                    className="form-control"
+                                    id="phoneNumber"
+                                    value={phoneNumber}
+                                    onChange={(e) => setPhoneNumber(e.target.value)} />
+                            </div>
+                            <div className="col-md-12">
+                                <label className="labels">Gender</label>
+                                <select
+                                    id="gender"
+                                    className="form-control"
+                                    value={gender}
+                                    onChange={(e) => setGender(e.target.value)}>
+                                    <option value="">Select Gender</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                            <div className="col-md-12">
+                                <label className="labels">Email</label>
+                                <input
+                                    name="userEmail"
+                                    type="email"
+                                    className="form-control"
+                                    id="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+                            <div className="col-md-12">
+                                <label className="labels">Department</label>
+                                <select
+                                    name="userDepartment"
+                                    type="text"
+                                    className="form-control"
+                                    id="department"
+                                    value={department}
+                                    onChange={(e) => setDepartment(e.target.value)}
+                                >
+                                    <option value="">Select Department</option>
+                                    <option value="Computer Engineering">Computer Engineering</option>
+                                    <option value="Bioengineering">Bioengineering</option>
+                                    <option value="Environmental Engineering">Environmental Engineering</option>
+                                    <option value="Electrical and Electronic Engineering">Electrical and Electronic Engineering</option>
+                                    <option value="Industrial Engineering">Industrial Engineering</option>
+                                    <option value="Civil Engineering">Civil Engineering</option>
+                                    <option value="Chemical Engineering">Chemical Engineering</option>
+                                    <option value="Mechanical Engineering">Mechanical Engineering</option>
+                                </select>
+                            </div>
 
-                            <div className="mt-5 text-center"><button className="btn btn-sm btn-primary profile-button mb-5" type="submit">Submit Changes</button></div>
+                            <div className="mt-5 text-center">
+                                <button className="btn btn-sm btn-primary profile-button mb-5" type="submit">Submit Changes</button>
+                            </div>
                             <a href="/Profile" className="btn btn-sm btn-dark mb-2">Back to Your Profile</a>
                         </form>
-
                     </div>
                 </div>
             </div>
