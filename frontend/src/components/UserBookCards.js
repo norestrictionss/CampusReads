8f689872204016d0dd3b3964c0f935123f43664b
irmaydin <irmaydin14@gmail.com>
2024-05-30 22:36:41 +0300

    css

--- frontend/src/components/UserBookCards.js
+++ frontend/src/components/UserBookCards.js
@@ -9,13 +9,13 @@ const UserBookCards = ({ title, author, image, bookID, userID }) => {
             <div className="card h-90">
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={image} style={{ width: "100px", height:"150px"}} className="img-fluid rounded-start" alt="..." />
+                        <img src={image} style={{ width: "100px"}} className="img-fluid rounded-start" alt="..." />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">Author : {author}</p>
-                            <a onClick={() => removeBookFromBooklist(userID, bookID)} className="btn btn-sm btn-dark mb-2" style={{ float: "center" }}>DELETE</a>
+                            <a onClick={() => removeBookFromBooklist(userID, bookID)} className="btn btn-sm btn-dark mb-2" style={{ float: "right" }}>DELETE</a>
                         </div>
                     </div>
                 </div>
